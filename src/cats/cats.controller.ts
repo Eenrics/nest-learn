14c943545e3ddb1089695a47d4cdf6ec4ca08b08
Ebenezer Exetie <ebenezeresh525@gmail.com>
2023-07-06 13:24:41 +0300

    more on inj of prov

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -3,6 +3,7 @@ import { CreateCatDto } from './dto/create-cat.dto'
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 import { ForbiddenException } from 'src/common/exceptions/forbidden.exception';
+import { User } from 'src/common/decorators/user.decorator';
 
 @Controller('cats')
 export class CatsController {
@@ -24,6 +25,11 @@ export class CatsController {
     redirect(@Query('q') q) {
         console.log('redirecting')
         if (q && q === 'google') return {url: 'https.www.google.com', statusCode: 300}
+    }
+
+    @Get('say-hi')
+    async sayHi(@User('firstName') firstName: string) {
+    console.log(`Hello ${firstName}`);
     }
 
     @Get(':id')
